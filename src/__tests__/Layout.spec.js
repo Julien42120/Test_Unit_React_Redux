b0ed62461175a50a6920d333f87ced799df681dd
Julien42120 <julienmartin42120@gmail.com>
2022-06-09 15:43:43 +0200

    test-Unit-React-Redux

---
+++ src/__tests__/Layout.spec.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme';
+
+import Layout from '../components/Layout';
+
+import '../setUpTests';
+
+//Créer un mock de Home
+
+describe('Layout snapshot',()=>{
+    it('It renders without crashing', () => {
+        //Créer le expect
+    });
+
+    it('It renders Home', () => {
+        //Créer snapshot
+    });
+});
---
+++ src/__tests__/calculatorActions.test.js
@@ -0,0 +1,17 @@
+import { addInputs , subtractInputs, fetchRandomNumber } from '../actions/calculatorActions'
+
+describe('ACTIONS - Test calculatorActions',()=>{
+
+    it('actionCreator addInputs', () => {
+        const add = addInputs(50);
+        //Créer le expect
+    });
+
+
+    it('actionCreator subtractInputs', () => {
+        const subtract = subtractInputs(-50);
+        //Créer le expect
+    });
+
+});
+
